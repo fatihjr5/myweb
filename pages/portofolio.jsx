d2c9566bcde10aff75ad73f954d75e7481a43f20
Fatih Ibrahim <shilla.ibra@gmail.com>
2022-04-28 10:11:22 +0700

    added portofolio

--- pages/portofolio.jsx
+++ pages/portofolio.jsx
@@ -16,7 +16,7 @@ function Portofolio() {
                 </section>
                 <a href="https://www.instagram.com/p/CZa_omtBRco/?utm_source=ig_web_copy_link" className='px-4 py-2 text-sm font-medium rounded-lg text-white bg-black'>See Detail</a>
             </div>
-            <div className="flex items-center justify-between pb-3">
+            <div className="flex flex-row items-start space-y-2 md:space-y-0 md:items-center justify-between pb-3">
                 <section className="flex flex-col">
                     <h5 className="text-base font-medium text-black mb-1">Stylish Detail Page</h5>
                     <p className="text-sm font-normal text-gray-500">UI Design</p>
@@ -43,6 +43,13 @@ function Portofolio() {
                     <p className="text-sm font-normal text-gray-500">Frontend Development</p>
                 </section>
                 <a href="https://manji-restaurant.netlify.app/" className='px-4 py-2 text-sm font-medium rounded-lg text-white bg-black'>See Detail</a>
+            </div>
+            <div className="flex items-center justify-between pb-3">
+                <section className="flex flex-col">
+                    <h5 className="text-base font-medium text-black mb-1">The Hyatt - UI Mobile Apps</h5>
+                    <p className="text-sm font-normal text-gray-500">UI Design</p>
+                </section>
+                <a href="https://www.instagram.com/p/Cc4P8SRBdqg/?utm_source=ig_web_copy_link" className='px-4 py-2 text-sm font-medium rounded-lg text-white bg-black'>See Detail</a>
             </div>
         </section>
     </Layouts>
